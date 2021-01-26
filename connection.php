960f987ed33cdbf44aaef93d0c9f371344297f27
vbpawar <vikaspawar3110@gmail.com>
2021-01-26 21:03:08 +0530

    add connection file

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 $server = 'localhost';
-$user   = 'root';
-$password = '';
-$dbname = 'spine_demo';
+$user   = 'tkinfote_myclinic';
+$password = 'Soulsoft@1987';
+$dbname = 'tkinfote_myclinic';
 $conn = new mysqli($server,$user,$password,$dbname) or die(mysqli_error($conn));
 ?>
