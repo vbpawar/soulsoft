e5144f8036e3e478346e013c9359718dc15f9070
vbpawar <vikaspawar3110@gmail.com>
2021-01-27 19:17:55 +0530

    add changes for file

--- connection.php
+++ connection.php
@@ -1,7 +1,12 @@
 <?php
+// $server = 'localhost';
+// $user   = 'tkinfote_myclinic';
+// $password = 'Soulsoft@1987';
+// $dbname = 'tkinfote_myclinic';
+// $conn = new mysqli($server,$user,$password,$dbname) or die(mysqli_error($conn));
 $server = 'localhost';
-$user   = 'tkinfote_myclinic';
-$password = 'Soulsoft@1987';
-$dbname = 'tkinfote_myclinic';
+$user   = 'root';
+$password = '';
+$dbname = 'spine_demo';
 $conn = new mysqli($server,$user,$password,$dbname) or die(mysqli_error($conn));
 ?>
