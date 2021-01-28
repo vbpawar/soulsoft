9c04422804a84cfb6670e0340660d4c592560897
lalit <lalit@DESKTOP-077QKLL>
2021-01-27 19:45:20 -0800

    setup connection with remote database

--- connection.php
+++ connection.php
@@ -4,9 +4,9 @@
 // $password = 'Soulsoft@1987';
 // $dbname = 'tkinfote_myclinic';
 // $conn = new mysqli($server,$user,$password,$dbname) or die(mysqli_error($conn));
-$server = 'localhost';
-$user   = 'root';
-$password = '';
-$dbname = 'spine_demo';
+$server = '208.109.12.251';
+$user   = 'soulsoft_myclinic';
+$password = 'Soulsoft@1987';
+$dbname = 'soulsoft_myclinic';
 $conn = new mysqli($server,$user,$password,$dbname) or die(mysqli_error($conn));
 ?>
