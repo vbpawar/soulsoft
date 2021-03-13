e516b1c3e2089c17e111fe4d2bc072190f33b797
lalit <lalit@DESKTOP-077QKLL>
2021-03-13 19:05:07 +0530

    contact us button 2

--- header.php
+++ header.php
@@ -212,7 +212,7 @@
                                  data-responsive_offset="on"
                                  data-start="2300">
                                 <div class="slide-content-box">
-                                    <a class="thm-btn" href="contact.html">contact us <i class="fa fa-sort-desc"></i></a>     
+                                    <a class="thm-btn" href="contact.php">contact us <i class="fa fa-sort-desc"></i></a>     
                                 </div>
                             </div>
                         </li>
@@ -247,7 +247,7 @@
                                  data-responsive_offset="on"
                                  data-start="2300">
                                 <div class="slide-content-box">
-                                    <a class="thm-btn" href="contact.html">contact us <i class="fa fa-sort-desc"></i></a>     
+                                    <a class="thm-btn" href="contact.php">contact us <i class="fa fa-sort-desc"></i></a>     
                                 </div>
                             </div>
                         </li>
